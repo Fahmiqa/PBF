043309e89081ff4995ea7708dc5ad99eebfda76f
Fahmiqa <fahmiquraisyabdillah@gmail.com>
2024-02-29 11:21:02 +0700

    3

--- praktikum3/src/app/page.tsx
+++ praktikum3/src/app/page.tsx
@@ -1,5 +1,27 @@
+// import Image from "next/image";
+// import Profile from "../components/profile";
+// export default function Home() {
+//     return (
+//       <div>
+//          <h2 className="text-center font-bold mb-4">Ilmuwan Yang Luar Biasa</h2>
+//         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
+//           <div className="flex justify-center">
+//             <Profile></Profile>
+//           </div>
+//           <div className="flex justify-center">
+//             <Profile></Profile>
+//           </div>
+//           <div className="flex justify-center">
+//             <Profile></Profile>
+//           </div>
+//         </div>
+//       </div>
+//   );
+// }
+
+
+
 import Image from "next/image";
-import Profile from "../components/profile";
 import { Gallery } from "@/components/gallery";
 export default function Home() {
     return (
@@ -7,13 +29,7 @@ export default function Home() {
          <h2 className="text-center font-bold mb-4">Ilmuwan Yang Luar Biasa</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
           <div className="flex justify-center">
-            <Profile></Profile>
-          </div>
-          <div className="flex justify-center">
-            <Profile></Profile>
-          </div>
-          <div className="flex justify-center">
-            <Profile></Profile>
+            <Gallery/>
           </div>
         </div>
       </div>
